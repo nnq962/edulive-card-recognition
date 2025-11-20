c41e33071a268f3ec53ac326ec97226c2ec63bb3
Quyết Nguyễn <quyetnguyen@NNQs-MacBook-Air.local>
2025-11-20 16:10:21 +0700

    update: clean

--- android-app/app/src/main/java/com/example/card_recognition/AppConfig.kt
+++ android-app/app/src/main/java/com/example/card_recognition/AppConfig.kt
@@ -2,10 +2,10 @@ package com.example.card_recognition
 
 object AppConfig {
     // Cấu hình cho Detector (Phát hiện đối tượng)
-    const val USE_NNAPI_FOR_DETECTOR = false
+    val USE_NNAPI_FOR_DETECTOR get() = false
     const val DETECTOR_THRESHOLD = 0.80f
 
     // Cấu hình cho Recognizer (Nhận diện thẻ)
-    const val USE_NNAPI_FOR_RECOGNIZER = false
+    val USE_NNAPI_FOR_RECOGNIZER get() = false
     const val RECOGNIZER_THRESHOLD = 0.80f
 }
